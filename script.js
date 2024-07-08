99f946f955a293b2f3f99cb731ecf4d85f152e6a
Reuben Thomas <1reubenthomas@gmail.com>
2024-07-08 16:43:39 -0400

    feat: edit JS to get pixels to resize based on new grid size

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
+const CANVAS_SIZE = 960;
+
 let gridContainer = document.querySelector("#grid-container");
 let gridSquares = [];
-for (let i = 0; i < 16 * 16; i++) {
-  gridSquares.push(document.createElement("div"));
-  gridSquares[i].style.width = "50px";
-  gridSquares[i].style.height = "50px";
-  gridSquares[i].style.outline = "2px solid red";
 
-  gridSquares[i].addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = "blue";
-  })
-  gridSquares[i].setAttribute("class", "grid-square");
-  gridContainer.appendChild(gridSquares[i]);
+let button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+  let newSize = Number.parseInt(prompt("New size? (1 - 100)"));
+  while (isNaN(newSize) || newSize > 100 || newSize < 0 ) {
+    if(!newSize){
+      return; //user pressed cancel
+    }
+    newSize = Number.parseInt(prompt("Invalid Size. (1 - 100)"));
+  }
+  deleteGrid();
+  createGrid(newSize)
+});
+
+
+function deleteGrid(){
+  gridSquares.forEach(element => {
+    element.remove();
+  });
 }
+
+function createGrid(size){
+  let boxSize = CANVAS_SIZE / size;
+
+  for (let i = 0; i < size * size; i++) {
+    gridSquares[i] = document.createElement("div");
+    gridSquares[i].style.width = `${boxSize}px`;
+    gridSquares[i].style.height = `${boxSize}px`;
+    gridSquares[i].setAttribute("class", "grid-square");
+
+    gridSquares[i].addEventListener('mouseover', (event) => {
+      event.target.style.backgroundColor = "blue";
+    })
+    
+    gridContainer.appendChild(gridSquares[i]);
+  }
+}
+
+
+createGrid(16);
+
+
 
