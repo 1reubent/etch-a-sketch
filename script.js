984668e22bf78683b900e0affa531159b0bff0ac
Reuben Thomas <1reubenthomas@gmail.com>
2024-07-08 17:28:14 -0400

    feat: add color buttons, center canvas

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const CANVAS_SIZE = 960;
 
+//COLORS
+const BLACK = "rgb(0,0,0)";
+const RED = "rgb(225,0,0)";
+const BLUE = "rgb(0,0,255)";
+const GREEN = "rgb(0,255,0)";
+let RAINBOW = false;
+let CURRENT_COLOR = BLACK;
+
+
+
 let gridContainer = document.querySelector("#grid-container");
 let gridSquares = [];
 
-let button = document.querySelector("button");
-button.addEventListener("click", (event) => {
-  let newSize = Number.parseInt(prompt("New size? (1 - 100)"));
-  while (isNaN(newSize) || newSize > 100 || newSize < 0 ) {
+//BUTTONS
+
+//SET GRID SIZE BUTTON
+let changeGridButton = document.querySelector("#change-grid");
+changeGridButton.addEventListener("click", () => {
+  let newSize = prompt("New size? (1 - 100)");
+  while (!newSize || isNaN(newSize) || newSize > 100 || newSize < 0 ) {
     if(!newSize){
       return; //user pressed cancel
     }
-    newSize = Number.parseInt(prompt("Invalid Size. (1 - 100)"));
+    newSize = prompt("Invalid size (1 - 100)");
   }
   deleteGrid();
   createGrid(newSize)
 });
 
+//SET COLOR BLACK BUTTON
+let colorBlackButton = document.querySelector("#color-black");
+colorBlackButton.addEventListener( "click", () => {
+  RAINBOW = false;
+  CURRENT_COLOR = BLACK;
+})
+
+//SET COLOR RED BUTTON
+let colorRedButton = document.querySelector("#color-red");
+colorRedButton.addEventListener( "click", () => {
+  RAINBOW = false;
+  CURRENT_COLOR = RED;
+})
+
+//SET COLOR BLUE BUTTON
+let colorBlueButton = document.querySelector("#color-blue");
+colorBlueButton.addEventListener( "click", () => {
+  RAINBOW = false;
+  CURRENT_COLOR = BLUE;
+})
+
+//SET COLOR GREEN BUTTON
+let colorGreenButton = document.querySelector("#color-green");
+colorGreenButton.addEventListener( "click", () => {
+  RAINBOW = false;
+  CURRENT_COLOR = GREEN;
+})
+
+//SET COLOR RAINBOW
+let colorRainbowButton = document.querySelector("#color-rainbow");
+colorRainbowButton.addEventListener( "click", () => {
+  RAINBOW = true;
+})
+
+
+
+//FUNCTIONS
 
 function deleteGrid(){
   gridSquares.forEach(element => {
@@ -32,8 +82,14 @@ function createGrid(size){
     gridSquares[i].style.height = `${boxSize}px`;
     gridSquares[i].setAttribute("class", "grid-square");
 
-    gridSquares[i].addEventListener('mouseover', (event) => {
-      event.target.style.backgroundColor = "blue";
+    gridSquares[i].addEventListener('mouseover', () => {
+      if (RAINBOW){
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        CURRENT_COLOR = `rgb(${r},${g},${b})`
+      }
+      event.target.style.backgroundColor = CURRENT_COLOR;
     })
     
     gridContainer.appendChild(gridSquares[i]);
